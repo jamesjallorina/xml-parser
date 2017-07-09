505927769a02cc150a7225ad82a9ae11a9219c49
James <jamesmarjun.jallorina@yahoo.com.ph>
2017-07-09 16:57:19 +0800

    cpp xml parser
    Signed-off-by: James <jamesmarjun.jallorina@yahoo.com.ph>

---
+++ xml/Utils.cpp
@@ -0,0 +1,110 @@
+// ===================================================================
+// AdvXMLParser
+//
+// See AdvXMLParser.h
+// ===================================================================
+
+#include "AdvXMLParser.h"
+#include<iostream.h>
+
+#if defined(_DEBUG) & defined(DEBUG_NEW)
+#undef THIS_FILE
+static char THIS_FILE[]=__FILE__;
+#define new DEBUG_NEW
+#endif
+
+// I prefer to use a namespace instead of "static" because the C++ standard
+// says that "static" is deprecated.
+namespace AdvXMLParser { namespace Private
+{
+
+// ===================================================================
+
+// White space: space, tabulation, carriage return, line feed.
+static const Char s_spaces[] = TEXT(" \t\r\n");
+
+void TrimSpaces(String& strText)
+// Remove leading and trailing spaces 
+{
+//////////////////////////////////
+//cout<<endl<<"EXIT TRIM SPACES"<<endl;
+if (strText.substr(0, 2) == ";;") // don't truncate but remove # from first char
+{
+	strText = strText.substr(2, strText.size());
+	return; 
+}
+//////////////////////////////////
+
+    // Seach spaces at the beginning
+    String::size_type nFirst = strText.find_first_not_of(s_spaces);
+    if(nFirst == String::npos)
+    {
+        // Only spaces. Empty the string
+        strText.erase();
+        return;
+    }
+
+    if(nFirst != 0) // Some spaces at the beginning. Remove them
+        strText.erase(0, nFirst);
+
+    // Search spaces at the end
+    String::size_type nLast = strText.find_last_not_of(s_spaces);
+    if(nLast != String::npos) // Some spaces at the end. Remove them
+        strText.erase(nLast + 1);
+}
+
+
+void NormalizeWhiteSpaces(String& strText) // static
+// Replace white spaces by only one space
+{
+//////////////////////////////////
+//cout<<endl<<"EXIT NORMALIXZE WHITE SPACES"<<endl;
+if (strText.substr(0, 2) == ";;") // don't truncate but remove # from first char
+{
+	strText = strText.substr(2, strText.size());
+	return; 
+}
+//////////////////////////////////
+
+    String::size_type nPosition = 0;
+    while(nPosition < strText.size())
+    {
+        String::size_type nFirst = strText.find_first_of(s_spaces, nPosition);
+        if(nFirst == String::npos)
+            break; // No space, so nothing to normalize
+
+        String::size_type nEnd = strText.find_first_not_of(s_spaces, nFirst);
+        if(nEnd == String::npos)
+        {
+            // Only spaces. Replace by only one space
+            strText.replace(nFirst, strText.size() - nFirst, 1, TEXT(' '));
+            break;
+        }
+
+        // Replace by only one space
+        strText.replace(nFirst, nEnd - nFirst, 1, TEXT(' '));
+        // New position: just after the space
+        nPosition = nFirst + 1;
+    }
+}
+
+void Normalize(String& strText) // static
+{
+//////////////////////////////////
+//cout<<endl<<"EXIT NORMALIXZE"<<endl;
+if (strText.substr(0, 2) == ";;") // don't truncate but remove # from first char
+{
+	strText = strText.substr(2, strText.size());
+	return; 
+}
+//////////////////////////////////
+
+    NormalizeWhiteSpaces(strText);
+    TrimSpaces(strText);
+}
+
+// ===================================================================
+
+} }
+
+
